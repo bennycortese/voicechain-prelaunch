4a17ed493089e70ecdc7209a6244fd3d71a9f0d3
bennycortese <bennycortese@gmail.com>
2024-06-20 00:26:49 -0700

    tts/tts_chain.py

--- misc/eleven_labs_test.py
+++ misc/eleven_labs_test.py
@@ -1,19 +1,22 @@
 from chains.tts.eleven_labs_tts_chain import ElevenLabsTTSChain
 from chains.tts import tts_chain
-from elevenlabs import save
+from dotenv import load_dotenv
+import os
 
 if __name__ == "__main__":
+    
+    load_dotenv()
 
-    if True:  # Set to True to run the test
-        api_key = ""
-        voice_id = ""
+    api_key = os.getenv("ELEVEN_LABS_API_KEY")
+    voice_id = os.getenv("ELEVEN_LABS_VOICE_ID")
+    print(api_key)
         
-        tts_chain = ElevenLabsTTSChain(api_key=api_key, voice_id=voice_id)
+    tts_chain = ElevenLabsTTSChain(api_key=api_key, voice_id=voice_id)
 
-        text_to_speech = "I like pineapple pizza!"
+    text_to_speech = "I like pineapple pizza!"
         
-        audio_content = tts_chain.generate_audio(text_to_speech)
+    audio_content = tts_chain.generate_audio(text_to_speech)
         
-        save(audio_content, "output.mp3") # Should likely introduce a default save abstraction so we can override it with this for eleven_labs
+    tts_chain.save_audio(audio_content, "eleven_labs_output.mp3")
         
-        print("Audio content has been saved to output.mp3")
+    print("Audio content has been saved to eleven_labs_output.mp3")
