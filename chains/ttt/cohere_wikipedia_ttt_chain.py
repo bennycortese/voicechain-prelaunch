c42dd68e33be7de6285601a850fff553c3888a15
bennycortese <bennycortese@gmail.com>
2024-06-24 02:29:58 -0700

    Added the first ttt chain, based around retrieving information from wikipedia using cohere RAG for the conversation

---
+++ chains/ttt/cohere_wikipedia_ttt_chain.py
@@ -0,0 +1,17 @@
+from langchain_cohere import CohereRagRetriever, ChatCohere
+from langchain.retrievers import WikipediaRetriever
+
+class CohereWikipediaTTTChain:
+    def __init__(self, api_key: str):
+        self.cohere_chat_model = ChatCohere(api_key=api_key)
+        self.wiki_retriever = WikipediaRetriever()
+        self.rag = CohereRagRetriever(llm=self.cohere_chat_model)
+
+    def _call(self, text_dict: dict) -> str:
+        text = text_dict['text']
+        wiki_docs = self.wiki_retriever.get_relevant_documents(text)
+        document_contents = "\n\n".join([doc.page_content for doc in wiki_docs])
+        query_with_context = f"{text}\n\n{document_contents}"
+        docs = self.rag.invoke(input=query_with_context)
+        response = docs[-1].page_content if docs else "No relevant documents found."
+        return {"text": response}
